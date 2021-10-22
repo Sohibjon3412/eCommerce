a23398a5494a87a5d13c87540ebe14b532632a43
Sohibjon3412 <Sohibjon34122@gmail.com>
2021-10-21 21:12:23 -0700

    commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let elMenu = document.querySelector(".header__menu-btn");
+let elModal = document.querySelector(".contact");
+
+elMenu.addEventListener("click",function(){
+  elModal.classList.toggle("contact__menu-size");
+  console.log("asdsdasd")
+});
