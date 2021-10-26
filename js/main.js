b8362f79402d597a8c69e3d7f93b2284611f2c15
Sohibjon3412 <Sohibjon34122@gmail.com>
2021-10-26 12:38:50 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -3,5 +3,33 @@ let elModal = document.querySelector(".contact");
 
 elMenu.addEventListener("click",function(){
   elModal.classList.toggle("contact__menu-size");
-  console.log("asdsdasd")
+});
+let elHeaderMenu = document.querySelector(".header__menu");
+let elLinethird = document.querySelector(".line2");
+let elLineOne = document.querySelector(".line1");
+let elLineThree = document.querySelector(".line3");
+let elNav = document.querySelector(".nav");
+let elHeaderTop = document.querySelector(".header-top");
+let elListbtn = document.querySelector(".fruits__btn--list");
+let elGridbtn = document.querySelector(".fruits__btn--grid");
+let elSmallList = document.querySelector(".categories__list--small");
+let elBiglList = document.querySelector(".categories__list--big");
+
+
+elHeaderMenu.addEventListener("click",function(){
+    elLinethird.classList.toggle("remove-line");
+    elLineOne.classList.toggle("rotate-line1");
+    elLineThree.classList.toggle("rotate-line3");
+    elNav.classList.toggle("open-nav");
+    elHeaderTop.classList.toggle("open-header");
+});
+
+elListbtn.addEventListener("click",function(){
+    elBiglList.classList.add("remove__list--big");
+    elSmallList.classList.add("show-list-small");
+});
+
+elGridbtn.addEventListener("click",function(){
+    elBiglList.classList.remove("remove__list--big");
+    elSmallList.classList.remove("show-list-small");
 });
